86e9953d4cd645ef9dec54387c0dde6c7d9f5774
32623ras <76033808+32623ras@users.noreply.github.com>
2020-12-24 14:12:23 +0500

    Program 1

---
+++ program 1.js
@@ -0,0 +1,6 @@
+var temprature=prompt("please enter temprature in celcius");
+console.log(temprature);
+var celciusTemprature=(parseInt(temprature)*(1.8))+32
+alert(celciusTemprature);
+console.log(celciusTemprature)
+
